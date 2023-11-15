c9b86fdc67e6a4b1661613af0dbcf1721aeb100e
bipinemp <bhandaribipin3.39@gmail.com>
2023-11-15 19:00:40 +0545

    changes

--- src/loading/ProductsLoading.tsx
+++ src/loading/ProductsLoading.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export function ProductsLoading() {
   const product = (
-    <div className="w-[280px] animate-pulse h-[280px] border-[1px] border-gray rounded-md px-2 pt-2 pb-2 flex flex-col gap-2">
-      <div className="relative bg-gray-300 rounded-md w-full h-[160px]"></div>
+    <div className="aspect-square w-full max-h-[310px] col-span-1 animate-pulse border-[1px] border-gray rounded-md px-2 pt-2 pb-2 flex flex-col gap-2">
+      <div className="relative w-full h-[200px] bg-gray-300 rounded-md"></div>
       <div className="px-2 flex flex-col gap-5 mt-1">
         <div className="flex flex-col gap-2">
           <h3 className="w-[150px] h-[20px] bg-gray-300 rounded-md"></h3>
@@ -17,7 +17,7 @@ export function ProductsLoading() {
     </div>
   );
   return (
-    <div className="grid grid-cols-4 gap-x-2 gap-y-5 mb-5">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-2 gap-y-5 mb-5">
       {product}
       {product}
       {product}
