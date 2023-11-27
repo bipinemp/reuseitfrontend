b4a2c436de4333f76af3c7422d65cc4ca3ab7de5
bipinemp <bhandaribipin3.39@gmail.com>
2023-11-27 22:06:38 +0545

    changes

--- src/components/profile/ProfileDetails.tsx
+++ src/components/profile/ProfileDetails.tsx
@@ -3,16 +3,18 @@
 import Image from "next/image";
 import Container from "../Container";
 import { useUserProfile } from "@/apis/queries";
-import { CalendarSearch } from "lucide-react";
+import { CalendarDays, CalendarSearch } from "lucide-react";
+import useFormatTime from "@/hooks/useFormatTime";
 
 const ProfileDetails: React.FC = () => {
   const { data, isPending } = useUserProfile();
   const imgurl = "http://127.0.0.1:8000/images/";
   const UserData: UserDetail | undefined = data;
+  const formattedDate = useFormatTime(UserData?.created_at || "");
 
   return (
     <Container>
-      <div className="flex justify-between mt-10">
+      <div className="flex gap-10 justify-between mt-10">
         <div className="flex flex-col gap-5 items-center w-[400px] bg-zinc-100 rounded-lg p-5">
           <div className="flex flex-col gap-2 items-center">
             <div className="relative w-[100px] h-[100px]">
@@ -26,12 +28,17 @@ const ProfileDetails: React.FC = () => {
             <h2 className="font-bold text-gray-700">{UserData?.name}</h2>
           </div>
           <div>
-            <p>
-              <CalendarSearch className="w-5 h-5" />
+            <p className="flex items-center gap-2 text-gray-600">
+              <span>
+                <CalendarDays className="w-[1.3rem] h-[1.3rem]" />
+              </span>
+              <span className="text-sm">Member since {formattedDate}</span>
             </p>
           </div>
         </div>
-        <div></div>
+        <div className="w-full">
+          <h1>No Posts right now ...</h1>
+        </div>
       </div>
     </Container>
   );
