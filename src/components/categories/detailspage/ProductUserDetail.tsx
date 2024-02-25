d9b1c0037f0978c6b1aa6fd2fd0e5dc01ca5079f
bipinemp <bhandaribipin3.39@gmail.com>
2024-02-25 21:16:48 +0545

    some optimization

--- src/components/categories/detailspage/ProductUserDetail.tsx
+++ src/components/categories/detailspage/ProductUserDetail.tsx
@@ -1,5 +1,8 @@
+"use client";
+
+import { useUserProfile } from "@/apis/queries";
 import { Button } from "@/components/ui/button";
-import { ChefHat, MessageCircleIcon } from "lucide-react";
+import { MessageCircleIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -15,11 +18,13 @@ const ProductUserDetail: React.FC<ProductUserProps> = ({
   image_url,
   name,
 }) => {
+  const { data: UserData } = useUserProfile();
+
   const imgurl = "http://127.0.0.1:8000/images/";
   return (
-    <div className="flex flex-col gap-4 border border-gray-400 bg-zinc-100 rounded-md py-3 px-4">
-      <div className="flex gap-4 items-center">
-        <div className="relative w-[60px] h-[60px]">
+    <div className="flex flex-col gap-4 rounded-md border border-gray-400 bg-zinc-100 px-4 py-3">
+      <div className="flex items-center gap-4">
+        <div className="relative h-[60px] w-[60px]">
           <Image
             fill
             className="rounded-full object-fill object-top"
@@ -27,17 +32,29 @@ const ProductUserDetail: React.FC<ProductUserProps> = ({
             src={imgurl + image_url}
           />
         </div>
-        <h3 className="font-semibold text-[1rem] sm:text-[1.17rem]">{name}</h3>
+        <h3 className="text-[1rem] font-semibold sm:text-[1.17rem]">{name}</h3>
       </div>
-      <Link href={`/user/${user_id}`}>
-        <Button
-          size="lg"
-          className="sm:text-lg font-semibold flex items-center gap-2"
-        >
-          <MessageCircleIcon className="w-5 h-5 sm:w-6 sm:h-6" />
-          Chat with seller
-        </Button>
-      </Link>
+      {UserData?.id ? (
+        <Link href={`/user/${user_id}`}>
+          <Button
+            size="lg"
+            className="flex items-center gap-2 font-semibold sm:text-lg"
+          >
+            <MessageCircleIcon className="h-5 w-5 sm:h-6 sm:w-6" />
+            Chat with seller
+          </Button>
+        </Link>
+      ) : (
+        <Link href={`/login`}>
+          <Button
+            size="lg"
+            className="flex items-center gap-2 font-semibold sm:text-lg"
+          >
+            <MessageCircleIcon className="h-5 w-5 sm:h-6 sm:w-6" />
+            Chat with seller
+          </Button>
+        </Link>
+      )}
     </div>
   );
 };
