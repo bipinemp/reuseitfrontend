26e5bb29d2609eb53b6daf62a9a1520c8d852284
bipinemp <bhandaribipin3.39@gmail.com>
2024-04-12 08:29:23 +0545

    changes

--- src/app/(auth)/admin/customers/page.tsx
+++ src/app/(auth)/admin/customers/page.tsx
@@ -56,7 +56,10 @@ const Page = () => {
         <div>
           {data &&
             data?.customers?.map((user: TUserDetail) => (
-              <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+              <div
+                key={user.id}
+                className="relative overflow-x-auto shadow-md sm:rounded-lg"
+              >
                 <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400 rtl:text-right">
                   <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
